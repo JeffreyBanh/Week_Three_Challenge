02e08dde20cdda293ec946405f2687d7f039f6f0
JeffreyBanh <banh.jeff@gmail.com>
2021-09-29 22:49:10 -0700

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,11 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+const lowercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz'.split('')
+const uppercase = 'abcdefhijklmnopqrstuvwxyz'.split('');
+const numeric = '0123456789'.split('');
+const specialCharacter = '!@#$%^&*'.split('');
+const newList = [lowercase, uppercase, numeric, specialCharacter];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
